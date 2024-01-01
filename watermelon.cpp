52347b379af7b7d56afd3e297edc3e5f6f46e075
shamilhossain <shamilbinhossain@gmail.com>
2024-01-02 01:33:50 +0600

    codeforces 800 problems watermelon

---
+++ watermelon.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main(){
+
+ int x;
+ scanf("%d",&x);
+if(x>=0 && x<=100){
+        if(x==2){
+            printf("NO");
+        }
+ else if(x%2==0){
+    printf("YES");
+ }
+ else
+    printf("NO");
+    }
+
+ return 0;
+
+
+ }
